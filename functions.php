05c6cbd3fabd5fb9a8650b6b52dff1137e77b51d
Kumiko <aoisora.hoshinoyoru.aoiumi@gmail.com>
2025-07-10 14:36:31 +0900

    0710_webP変換、常時SSL（functions.php）

--- functions.php
+++ functions.php
@@ -211,3 +211,9 @@ function register_custom_taxonomy_news() {
 }
 add_action('init', 'register_custom_taxonomy_news');
 
+// SSLリダイレクト（常時SSL対応）
+if (empty($_SERVER['HTTPS'])) {
+	header("HTTP/1.1 301 Moved Permanently");
+	header("Location: https://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}");
+	exit;
+}
